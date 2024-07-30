5ee21b316b2570e96312ad9cafcec1193f702824
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2024-07-30 11:11:57 -0400

    f   - updated cht.sh to work with java commands and linked source repo

--- tmux/cht.sh
+++ tmux/cht.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+# Utilizes cht.sh tool found here: https://github.com/chubin/cheat.sh
+
 languages=$(echo "golang c cpp typescript rust python" | tr " " "\n")
-core_utils=$(echo "find xargs sed awk" | tr " " "\n")
+core_utils=$(echo "java find xargs sed awk" | tr " " "\n")
 selected=$(echo -e "$languages\n$core_utils" | fzf)
 
 read -p "GIMME YOUR QURERY: " query
